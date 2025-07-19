e6b4e1951c6e026e4031262ae101fecd2c391c2e
Diego <droxo63@gmail.com>
2025-07-18 21:23:16 -0300

    Corrige erro no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,14 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br',
-    specPattern: 'tests/UI/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}, 
+    specPattern: 'tests/UI/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false
   },
 
   // Configuração do Mochawesome
   reporter: 'mochawesome',
   reporterOptions: {
-    // pasta onde os relatórios serão gerados
-    reportDir: 'reports/ui',   
+    reportDir: 'reports/ui',   // pasta onde os relatórios serão gerados
     overwrite: false,
     html: true,
     json: true,
