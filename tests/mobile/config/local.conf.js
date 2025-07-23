53341c6a794e0afc4cd37153e8f5cf65c05ecf34
Diego <droxo63@gmail.com>
2025-07-22 21:27:35 -0300

    app

--- tests/mobile/config/local.conf.js
+++ tests/mobile/config/local.conf.js
@@ -3,14 +3,20 @@ import { generalConf } from './general.conf.js';
 export let localConf = {
   runner: 'local',
   port: 4723,
-  capabilities: [{
-    platformName: 'Android',
-    'appium:deviceName': 'Android Emulator',
-    'appium:platformVersion': '12.0',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': `${process.cwd()}/app/ebacshop.apk`,
-    'appium:appWaitActivity': '.MainActivity',
-    'appium:disableIdAutoCompletion': true
-  }],
+
+capabilities: [{
+  platformName: 'Android',
+  'appium:deviceName': 'emulator-5554',
+  'appium:platformVersion': '14.0',
+  'appium:automationName': 'UiAutomator2',
+  'appium:app': `${process.cwd()}/app/loja-ebac.apk`,
+  'appium:appPackage': 'com.woocommerce.android',
+  'appium:appActivity': 'com.woocommerce.android.ui.main.MainActivity',
+  'appium:disableIdAutoCompletion': true
+}],
+
+
+  // se tiver hooks, timeouts ou reporter, coloque aqui...
+
   ...generalConf
 };
